9637e3c49998f1ac8f68c2ae66637340379b8ce6
ccs <chaichuanshi@sunlands.com>
2018-12-25 21:04:38 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,13 @@
+setCookie("username","admin");
+setCookie("passwd","admin123");
+
+function login(){
+        var userName=document.getElementById("userName").value;
+        var passwd=document.getElementById("passwd").value;
+
+        if(userName==getCookie("username")&&passwd==getCookie("passwd")){
+            location.href="main.html";
+        }else{
+            alert("用户名或者密码不对");
+        }
+};
---
+++ js/main.js
@@ -0,0 +1,4 @@
+
+function play(){
+    window.open("petvideo.html", "_blank", 'width=530,height=858,top=5px,left=5px');
+}
