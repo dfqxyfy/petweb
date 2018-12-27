7f3bc62e63627223ab2b955fc8278d4b15745aa4
dfqxyfy <crazyanyone@163.com>
2018-12-28 00:25:14 +0800

    基本demo

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
 setCookie("username","admin");
 setCookie("passwd","admin123");
 
+
 function login(){
         var userName=document.getElementById("userName").value;
         var passwd=document.getElementById("passwd").value;
 
-        if(userName==getCookie("username")&&passwd==getCookie("passwd")){
+        //兼容local,不能设置cookie的场景
+        var storeUser=getCookie("username");
+        var storePasswd=getCookie("passwd");
+        if(storeUser==null){
+            storeUser="admin";
+        }
+        if(storePasswd==null){
+            storePasswd="admin123";
+        }
+
+        if(userName==storeUser&&passwd==storePasswd){
             location.href="main.html";
         }else{
             alert("用户名或者密码不对");
