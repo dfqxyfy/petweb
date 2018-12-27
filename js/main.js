4bdfeacfef86d06900d3b8cd0fcbf2eb0b23a8a1
ccs <chaichuanshi@sunlands.com>
2018-12-27 19:40:20 +0800

    password

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var spread=true;
+var spread=false;
 function hideDiv(obj) {
     var ele = obj.parentNode.parentNode.children[1];
-    var read = "显示 »";
+    var read = "阅读全文 »";
     if (spread) {
         ele.setAttribute("style", "display:none");
     }else {
-        read = "隐藏 "
+        read = "隐藏 «"
         ele.setAttribute("style","");
     }
     obj.innerText=read;
