0dc4e54efadd8e41da6b33b6b800a65f48fc5ea7
ccs <chaichuanshi@sunlands.com>
2018-12-28 15:27:32 +0800

    password

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ var opt = null;
 var after=0;
 var needClear=false;
 window.onload = function(){
+    initStatus();
     var btns=document.getElementById('computer');
     for(i=0;i<btns.childElementCount;i++){
         var childNodes = btns.children[i].childNodes;
@@ -97,4 +98,31 @@ function spand(){
         document.getElementById("computer").setAttribute("style","");
     }
     spandFlag=!spandFlag;
+}
+
+//项目二
+function initStatus(){
+    var userName=getCookie("userName");
+    window.defaultStatus=userName+" 登陆了";
+}
+
+function getViewportSize(w){
+    w= w || window;
+    //除了IE8以及更早版本外，其它浏览器都能用
+    if(w.innerWidth != null){ return { w:w.innerWidth, h:w.innerHeight } };
+
+    //对标准模式下的IE或任何浏览器
+    var d=w.document;
+    if(document.compatMode == "CSS1Compat"){
+        return { w:d.documentElement.clientWidth,
+            h:d.documentElement.clientHeight }
+    }
+
+    //对怪异模式下的浏览器
+    return { w:d.body.clientWidth , h:d.body.clientHeight }
+
+}
+function displayView() {
+    var obj=getViewportSize(this.window);
+    alert("宽度："+obj.w +"\r\n"+"高度："+obj.h)
 }
